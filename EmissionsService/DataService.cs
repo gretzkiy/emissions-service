b2e61ae0d632a33cf15ab57f130fd4c2db9bc4c8
Doonalduck <katyaogareva@gmail.com>
2019-12-06 00:18:28 +0300

    Тестовый сервис, который пишет в тестовую БД

---
+++ EmissionsService/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+
+namespace EmissionsService
+{
+    class DataService
+    {
+        const string DbConnectionString = @"Server=.\SQLEXPRESS;Database=emissionsDBtest;Trusted_Connection=True;";
+        
+        public static void WriteNum(int num)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(DbConnectionString))
+            {
+                var sqlQuery = "INSERT INTO MySources (SourceUuid, pniv) VALUES (@SourceUuid, @pniv);";
+
+                connection.Execute(sqlQuery, new { pniv = num, SourceUuid = Guid.NewGuid().ToString() });
+            }
+        }
+    }
+}

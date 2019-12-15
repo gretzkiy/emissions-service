2eb3f106b8064e6cae2848d01ef81ff842b74a96
stepanernest <stepan-ernest@mail.ru>
2019-12-15 23:08:20 +0300

    finish

--- EmissionsService/DBService.cs
+++ EmissionsService/DBService.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using EmissionsLibrary;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EmissionsLibrary;
 using System.Configuration;
 
 namespace EmissionsService
--- EmissionsService/TransmitionService.cs
+++ EmissionsService/TransmitionService.cs
@@ -1,17 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using EmissionsLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Dapper;
-using System.Data;
-using System.Configuration;
-using EmissionsLibrary;
-using System.Net;
-using System.Net.Http;
-using System.Runtime.Serialization.Formatters.Soap;
-using System.IO;
-using AutoMapper;
 
 namespace EmissionsService
 {
